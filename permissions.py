2b31615bb36876301ae90ef03f2ce6e5db8b34d8
arafatxmals-code <arafatxmals@gmail.com>
2026-08-13 14:59:08 +0600

    Delete permissions.py

--- permissions.py
+++
@@ -1,6 +0,0 @@
-from database import get_role
-LEVEL={"user":0,"moderator":1,"admin":2,"ceo":3,"owner":4}
-def level(uid): return LEVEL.get(get_role(uid),0)
-def is_owner(uid): return level(uid)>=4
-def can_manage_stars(uid): return level(uid)>=2
-def can_manage_players(uid): return level(uid)>=2
